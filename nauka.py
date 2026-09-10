5928da5d70f8a219dd9bd3e9a58d459937d8203c
Łukasz Bułdak <posterwad@gmail.com>
2026-09-10 16:29:46 +0200

    isnakndcba23fivwrenkqcdsvif1

--- nauka.py
+++ nauka.py
@@ -425,13 +425,14 @@ import random
 # print(f"Średnia: {srednia(lista_liczba)}")
 # print(f"Max liczba: {max(lista_liczba)}, min liczba: {min(lista_liczba)}")
 
-def suma_parzystych(lista):
-    suma = 0
-    for liczba in lista:
-        if liczba % 2 == 0:
-            suma += liczba
-    return suma
+#def suma_parzystych(lista):
+#    suma = 0
+#    for liczba in lista:
+#        if liczba % 2 == 0:
+#            suma += liczba
+#    return suma
 
-lista_liczb = [1,2,3,4,5,6,7,8,9,10]
+#lista_liczb = [1,2,3,4,5,6,7,8,9,10]
 
-print(f"Suma liczb parzystych: {suma_parzystych(lista_liczb)}")
+#print(f"Suma liczb parzystych: {suma_parzystych(lista_liczb)}")
+
